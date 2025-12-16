e69aa6a11d25a03e4e3c8326892a66082b09dbf5
adarshshivan <adarshshivan30@gmail.com>
2025-12-16 18:44:35 +0530

    Ensure PEP8-compliant spacing for flake8 linting

--- app/main.py
+++ app/main.py
@@ -5,10 +5,12 @@ logging.basicConfig(
     format="%(asctime)s | %(levelname)s | %(message)s"
 )
 
+
 def run():
     logging.info("Application started")
     logging.info("Performing basic validation logic")
     logging.info("Application completed successfully")
+
 
 if __name__ == "__main__":
     run()
